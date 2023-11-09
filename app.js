cbcb70c47d09b38049e4d7f7ef91c14988ad2b2e
ElHoS <73666281+Ahmed-Elhosiny@users.noreply.github.com>
2023-11-09 06:50:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const notFound = require('./middlewares/not-found');
 const errorHandler = require('./middlewares/error-handler');
 const app = express();
 
-// MIDDLEWARE
+
 app.use(express.json());
 app.use(express.static('./public'));
 app.use(morgan('dev'));
 
-// ROUTES
+
 app.use('/api/v1/tasks', tasksRouter);
 
 app.use(notFound);
